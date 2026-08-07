b0a3032ec1a06cd064791f70223afd7e51636a12
AndreyWebX <andreistate77@gmail.com>
2026-08-07 16:00:49 +0300

    main

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function calculation() {
     let price = Number(document.getElementById("price").value);
     let tip = Number(document.getElementById("tip").value);
-    let priceTip = (tip / 100) * price;
-    document.getElementById("priceTip").textContent = "Pret Bacsis: " + priceTip;
-    let total = price + priceTip;
+    let PriceTip = (tip / 100) * price;
+    document.getElementById("priceTip").textContent = "Pret bacsis: " + PriceTip;
+    let total = price + PriceTip;
     document.getElementById("total-p").textContent = "Total: " + total;
 }
 
 function reset() {
-    document.getElementById("price").value = "";
-    document.getElementById("tip").value = "";
-    document.getElementById("priceTip").textContent = "Pret Bacsis: ";
-    document.getElementById("total-p").textContent = "Total: ";
+    Number((document.getElementById("price").value = ""));
+    Number((document.getElementById("tip").value = ""));
+    document.getElementById("priceTip").textContent = "Pret bacsis: ";
+    document.getElementById("total-p").textContent = "Total";
 }
